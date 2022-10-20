546a9a5cb61047e2845fa3a04de443f46ca643b6
Charbel-Francis <charbel.ff@gmail.com>
2022-10-20 13:13:47 +0200

    Changes made of dark mode and light mode

--- js/switch.js
+++ js/switch.js
@@ -2,26 +2,6 @@ jQuery(document).ready(function ($) {
 
   $('#toggle-box-checkbox').on('change', function () {
     if (this.checked) {
-      $('body').removeClass('night');
-      $('blog-entry .text .heading a:hover').removeClass('night');
-      $('blog-entry .text .heading a:focus').removeClass('night');
-      $('blog-entry .text .heading a:active').removeClass('night');
-      $('blog-entry .text .heading.a').removeClass('night');
-      // $('a.nav-link span').removeClass('night');
-      $(".turn-white").removeClass("white_n");
-      $(".turn-white").addClass("night_n");
-      $('p').removeClass('night');
-      $('h1').removeClass('night');
-      $('h2').removeClass('night');
-      $('h3').removeClass('night');
-      $('h4').removeClass('night');
-      $('h5').removeClass('night');
-      $('h6').removeClass('night');
-      $('.progress-bar span').removeClass('night');
-      $('span.subheading').removeClass('night');
-    } else {
-      
-      
       $('body').addClass('night');
       $('blog-entry .text .heading a:hover').addClass('night');
       $('blog-entry .text .heading a:focus').addClass('night');
@@ -39,27 +19,27 @@ jQuery(document).ready(function ($) {
       $('h6').addClass('night');
       $('.progress-bar span').addClass('night');
       $('span.subheading').addClass('night');
+    } else {
+      $('body').removeClass('night');
+      $('blog-entry .text .heading a:hover').removeClass('night');
+      $('blog-entry .text .heading a:focus').removeClass('night');
+      $('blog-entry .text .heading a:active').removeClass('night');
+      $('blog-entry .text .heading.a').removeClass('night');
+      // $('a.nav-link span').removeClass('night');
+      $(".turn-white").removeClass("white_n");
+      $(".turn-white").addClass("night_n");
+      $('p').removeClass('night');
+      $('h1').removeClass('night');
+      $('h2').removeClass('night');
+      $('h3').removeClass('night');
+      $('h4').removeClass('night');
+      $('h5').removeClass('night');
+      $('h6').removeClass('night');
+      $('.progress-bar span').removeClass('night');
+      $('span.subheading').removeClass('night');
     }
   });
-  $( document ).ready(function() {
-    $('body').addClass('night');
-    $('blog-entry .text .heading a:hover').addClass('night');
-    $('blog-entry .text .heading a:focus').addClass('night');
-    $('blog-entry .text .heading a:active').addClass('night');
-    $('blog-entry .text .heading.a').addClass('night');
-    $(".turn-white").addClass("white_n");
-    $(".turn-white").removeClass("night_n");
-    // $('a.nav-link span').addClass('night');
-    $('p').addClass('night');
-    $('h1').addClass('night');
-    $('h2').addClass('night');
-    $('h3').addClass('night');
-    $('h4').addClass('night');
-    $('h5').addClass('night');
-    $('h6').addClass('night');
-    $('.progress-bar span').addClass('night');
-    $('span.subheading').addClass('night');
-});
+
   // run demo if in searchresult preview
   function demo() {
     setInterval(function () {
@@ -77,11 +57,11 @@ jQuery(document).ready(function ($) {
 $(document).ready(function () {
   $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    if (scroll ==0) {
+    if (scroll > 0) {
         $(".turn-white").addClass("night_nav");
         $(".turn-white").removeClass("night");
     }
-    else if(scroll > 0){
+    else if(scroll==0){
       if ($("#toggle-box-checkbox").is(':checked')) {
         $(".turn-white").addClass("white_n");
         $(".turn-white").removeClass("night_nav");
